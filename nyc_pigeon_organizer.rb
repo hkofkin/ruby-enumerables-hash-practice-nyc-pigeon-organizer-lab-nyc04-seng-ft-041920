d77909625471cc229370b96726441cacfd44d666
Hannah Kofkin <hkofkin@gmail.com>
2020-05-02 01:52:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,11 +8,26 @@ def nyc_pigeon_organizer(data)
   data.each do |attribute, set_of_attributes|
     set_of_attributes.each do |detail, names|
       names.each do |name|
-        sorted_hash[name] ||= {}
-        sorted_hash[name][attribute] ||= []
-        sorted_hash[name][attribute].push(detail.to_s)
+        sorted_hash[name] = {}
       end
     end
   end
+
+  data.each do |attribute, set_of_attributes|
+    set_of_attributes.each do |detail, names|
+      names.each do |name|
+        sorted_hash[name][attribute] = []
+      end
+    end
+  end
+  
+  data.each do |attribute, set_of_attributes|
+    set_of_attributes.each do |detail, names|
+      names.each do |name|
+        sorted_hash[name][attribute] << detail.to_s
+      end
+    end
+  end
+  
   sorted_hash
 end
