604cf701cdbc99821260d02bfc75c1b505ea0946
Hannah Kofkin <hkofkin@gmail.com>
2020-05-02 02:00:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,8 +11,8 @@ def nyc_pigeon_organizer(data)
         sorted_hash[name] = {}
       end
     end
-    binding.pry
   end
+binding.pry 
 
   data.each do |attribute, set_of_attributes|
     set_of_attributes.each do |detail, names|
